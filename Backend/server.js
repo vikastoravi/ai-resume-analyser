b3a2332214c5cf2b03339d097db99baf8558ab07
vikastoravi <vikastoravi18@gmail.com>
2026-05-17 12:51:58 +0530

    Fixed Render CORS issue

--- Backend/server.js
+++ Backend/server.js
@@ -1,16 +1,8 @@
 require("dotenv").config()
 const app = require("./src/app")
 const connectToDB = require("./src/config/database")
-const cors = require("cors");
 
 connectToDB()
-app.use(
-  cors({
-    origin: "https://ai-resume-analyser-frontend-49oe.onrender.com",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-); 
 
 
 const PORT = process.env.PORT || 5000;
--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://ai-resume-analyser-frontend-49oe.onrender.com",
     credentials: true
 }))
 
